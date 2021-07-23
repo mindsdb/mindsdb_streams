360356007d4cf585582e3d6a195b2340995f7aa2
Ilya Tsyplenkov <ilya@mindsdb.com>
2021-07-23 12:46:23 +0300

    Adding setup info

---
+++ streams/__about__.py
@@ -0,0 +1,10 @@
+__title__ = "MindsDB"
+__package_name__ = 'mindsdb_streams'
+__version__ = '0.0.1'
+__description__ = "MindsDB Streams, provide streams capabilities to mindsdb native python library"
+__email__ = "jorge@mindsdb.com"
+__author__ = 'MindsDB Inc'
+__github__ = 'https://github.com/mindsdb/mindsdb_streams'
+__pypi__ = 'https://pypi.org/project/mindsdb_streams/'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyright 2018- mindsdb'
