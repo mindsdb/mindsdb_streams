060b54a79240344213777e8c7e69fa41d4a2926f
Ilya Tsyplenkov <ilya@mindsdb.com>
2021-08-10 18:16:25 +0300

    Change cmd params to required

--- make_stream.py
+++ make_stream.py
@@ -5,12 +5,12 @@ from streams import KafkaStream, RedisStream, StreamController
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--connection_info', type=str, help="json string with connection info", required=True)
-parser.add_argument('--predictor', type=str, help="predictor model name", required=True)
-parser.add_argument('--input_stream', type=str, required=True)
-parser.add_argument('--output_stream', type=str, required=True)
-parser.add_argument('--anomaly_stream', type=str, default=None)
-parser.add_argument('--type', type=str.lower, choices=['kafka', 'redis'], required=True)
+parser.add_argument('connection_info', type=str, help="json string with connection info")
+parser.add_argument('predictor', type=str, help="predictor model name")
+parser.add_argument('input_stream', type=str)
+parser.add_argument('output_stream', type=str)
+parser.add_argument('anomaly_stream', type=str)
+parser.add_argument('type', type=str.lower, choices=['kafka', 'redis'])
 
 
 if __name__ == '__main__':
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     stream_class = RedisStream if args.type == 'redis' else KafkaStream
     stream_out = stream_class(args.output_stream, **connection_info)
     stream_in = stream_class(args.input_stream, **connection_info)
-    stream_anomaly = stream_class(args.anomaly_stream, **connection_info) if args.anomaly_stream else None
+    stream_anomaly = stream_class(args.anomaly_stream, **connection_info) if args.anomaly_stream not in ('', None, 'None', 'none') else None
     controller = StreamController(args.predictor, stream_in, stream_out, stream_anomaly)
 
     controller.work()
